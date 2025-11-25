06770dadccd81396a9fa19ea873325c85789d3b2
Roar Bakken Stovner <robast@oslomet.no>
2025-11-25 10:48:02 +0100

    Fix an error that hid errors from validate_masters

--- data-raw/masters.R
+++ data-raw/masters.R
@@ -12,14 +12,17 @@ filenames <- filenames[!str_detect(filenames, "hivolda")]  # dropp den korrupte 
 # HIOF
 hiof_files <- filenames[str_detect(filenames, "hiof")]
 hiof <- map(hiof_files, process_hiof) |> bind_rows()
+validate_masters(hiof)
 
 # HVL
 hvl_files <- filenames[str_detect(filenames, "hvl")]
 hvl <- map(hvl_files, process_hvl) |> bind_rows()
+validate_masters(hvl)
 
 # INN
 inn_files <- filenames[str_detect(filenames, "inn_")]
 inn <- map(inn_files, process_inn) |> bind_rows()
+validate_masters(inn)
 
 # OSLOMET – gamle og nye
 oslomet_old_files <- filenames[str_detect(filenames, "oslomet_.*5-10")]
@@ -27,18 +30,23 @@ oslomet_new_files <- filenames[str_detect(filenames, "oslomet_.*1-7")]
 
 oslomet <- map(oslomet_old_files, process_oslomet_old) |> bind_rows()
 oslomet_new <- map(oslomet_new_files, process_oslomet_new) |> bind_rows()
+validate_masters(oslomet)
+validate_masters(oslomet_new)
 
 # USN 
 usn_files <- filenames[str_detect(filenames, "usn_")]
 usn <- map(usn_files, process_usn) |> bind_rows()
+validate_masters(usn)
 
 # UiA 
 uia_files <- filenames[str_detect(filenames, "uia_")]
 uia <- map(uia_files, process_uia) |> bind_rows()
+validate_masters(uia)
 
 # UiT
 uit_files <- filenames[str_detect(filenames, "UiT_")]
 uit <- map(uit_files, process_uit) |> bind_rows()
+validate_masters(uit)
 
 
 masters <- list(
